5b2fbe339bac6dd7ea8fd6a95457c9937c24fe82
Isaac Scheff <isaac1996@comcast.net>
2022-02-15 08:33:28 -0500

    CORS reset

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors({
-  origin: ['https://ttrpgcampaigntracker.herokuapp.com/']
-}));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
