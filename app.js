d1be78d1780b0adeadf47bd34e83ec648e30a6c2
Isaac Scheff <isaac1996@comcast.net>
2022-02-15 14:34:43 -0500

    added whitelist

--- app.js
+++ app.js
@@ -17,7 +17,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors({origin: 'http://frontend.campaigntracker.org'}));
+
+const whitelist = ["http://frontend.campaigntracker.org"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
