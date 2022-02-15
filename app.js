8caf2fb83db8da42a044a03758e1f1bb14f06592
Isaac Scheff <isaac1996@comcast.net>
2022-02-14 19:11:06 -0500

    Added heroku frontend to allowed cors list

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://ttrpgcampaigntracker.herokuapp.com/']
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
